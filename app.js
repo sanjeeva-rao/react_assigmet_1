d2c87001211d7f69eab9c395d901624e7fa64e8e
Sanjeeva Rao <x1104509@ti.com>
2024-07-21 10:45:51 +0530

    multiple tags using react

--- app.js
+++ app.js
@@ -7,3 +7,8 @@ document.getElementById('root').appendChild(root);
 const heading = React.createElement('h1', {id: 'heading'}, 'Hello world using react');
 const reactRoot = ReactDOM.createRoot(document.getElementById('reactRoot'));
 reactRoot.render(heading);
+
+//Add multiple tags
+const headings = [React.createElement('h1', {}, "Heading 1"), React.createElement('h2', {}, "Heading 2")];
+const multipleRoot = ReactDOM.createRoot(document.getElementById('multipleRoot'));
+multipleRoot.render(headings)
