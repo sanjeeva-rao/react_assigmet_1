046a5c8ea0ee8d53d99da2bfa83dcea694cf37a7
Sanjeeva Rao <x1104509@ti.com>
2024-07-21 10:45:09 +0530

    Hello world using react

--- app.js
+++ app.js
@@ -2,3 +2,8 @@
 const root = document.createElement('h1');
 root.innerHTML = 'Hello world Using JS';
 document.getElementById('root').appendChild(root);
+
+//using react
+const heading = React.createElement('h1', {id: 'heading'}, 'Hello world using react');
+const reactRoot = ReactDOM.createRoot(document.getElementById('reactRoot'));
+reactRoot.render(heading);
